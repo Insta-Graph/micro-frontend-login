39277263aff0d4dec5c39c49949f9a2dba1f278e
Edward Ramirez <Edward.Ramirez@coxautoinc.com>
2022-09-26 14:49:03 -0500

    fix: handle errors with common error boundary

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -2,26 +2,34 @@ import React, { Suspense } from 'react';
 
 import { ThemeProvider as MaterialThemeProvider } from '@mui/material';
 import { Router, Redirect } from '@reach/router';
-import { ThemeProvider, createCustomTheme, ApolloProvider, Loader } from '@snapify/shared-modules';
+import {
+  ThemeProvider,
+  createCustomTheme,
+  ApolloProvider,
+  Loader,
+  ErrorBoundary,
+} from '@snapify/shared-modules';
 
 const Login = React.lazy(() => import('./components/login'));
 const Register = React.lazy(() => import('./components/register'));
 
 const Root: React.FC = () => {
   return (
-    <ApolloProvider>
-      <MaterialThemeProvider theme={createCustomTheme()}>
-        <ThemeProvider>
-          <Suspense fallback={<Loader />}>
-            <Router basepath="/auth">
-              <Redirect from="/" to="/auth/sign-in" />
-              <Register path="/sign-up" />
-              <Login default />
-            </Router>
-          </Suspense>
-        </ThemeProvider>
-      </MaterialThemeProvider>
-    </ApolloProvider>
+    <ErrorBoundary>
+      <ApolloProvider>
+        <MaterialThemeProvider theme={createCustomTheme()}>
+          <ThemeProvider>
+            <Suspense fallback={<Loader />}>
+              <Router basepath="/auth">
+                <Redirect from="/" to="/auth/sign-in" />
+                <Register path="/sign-up" />
+                <Login default />
+              </Router>
+            </Suspense>
+          </ThemeProvider>
+        </MaterialThemeProvider>
+      </ApolloProvider>
+    </ErrorBoundary>
   );
 };
 
